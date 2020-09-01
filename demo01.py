3aab96aafb97ba8e1cfa59c81f10bbe275db7e3b
wuqin <593653197@qq.com>
2020-09-01 23:21:00 +0800

    3333

--- demo01.py
+++ demo01.py
@@ -1,4 +1,17 @@
-a = input('请输入：')
+"""a = input('请输入：')
 b = input('请输入：')
 print('长度是:',len(a + b))
-print("woaiwuqin" * 10)
+print("woaiwuqin" * 10)"""
+a = (1,2,3,4,"哈哈","呵呵",True,False)
+#print(a[4])
+print(a.index("呵呵"))  
+print()
+print(a.count(3))
+
+
+
+b = a.get(2)
+c = a.pop[2]
+print(a)
+
+print(b)
---
+++ demo02.py
@@ -0,0 +1,8 @@
+a = input("请输入姓名：")
+
+b = input("请输入年龄：")
+
+c = input("请输入性别：")
+
+d = {"name":a,"age":b,"sex":c}
+print(d)
