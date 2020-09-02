47f9bdea2b7265fa141faa3ee2bb9cff56fa2190
wuqin <593653197@qq.com>
2020-09-02 23:33:37 +0800

    444

---
+++ demo03.py
@@ -0,0 +1,38 @@
+"""a = 1  
+b = 2
+if a > b:
+    print("a更大")
+else:
+    print("b更大")
+
+
+    age = int(input("请输入你的年龄："))
+
+    if age > 60:
+        print("你应该退休了")
+    elif age > 30:
+        print("家庭责任很重把！")
+    elif age > 20:
+        print("一定要好好规划你的未来！")
+    else:
+        print("读书的时候要认真！")"""
+
+
+
+"""a = input("请输入：") 
+if a in "0123456789":
+    a = int(a)
+    print(a)
+else:
+    print("请输入正确的值")
+    exit()"""
+
+
+
+a = ["我","shi","ni","die"]
+for i in a:
+    print(i)
+
+a = list(range(2,50))
+for i in a:
+    print(i)
---
+++ honglvdeng.py
@@ -0,0 +1,19 @@
+b = 0
+while b < 5:
+    b += 1
+    a = 30
+    while a <= 30 and a > 0:
+            print("红灯还有"+str(a)+"秒结束")
+            a = a - 1
+    a = 3
+    while a <= 3 and a > 0:
+            print("黄灯还有"+str(a)+"秒结束")
+            a = a - 1
+    a = 35
+    while a <= 35 and a > 0:
+            print("绿灯还有"+str(a)+"秒结束")
+            a = a - 1
+    a = 3
+    while a <= 3 and a > 0:
+            print("黄灯还有"+str(a)+"秒结束")
+            a = a - 1
